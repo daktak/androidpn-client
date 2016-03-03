c72d79bdd3d53ebcab6267fddc7587c0b190df1f
daktak <daktak@gmail.com>
2016-03-03 16:07:12 +1100

    linting

--- app/src/main/java/org/androidpn/client/SerivceManager/NotificationService.java
+++ app/src/main/java/org/androidpn/client/SerivceManager/NotificationService.java
@@ -94,7 +94,7 @@ public class NotificationService extends Service {
         // Log.d(LOGTAG, "deviceId=" + deviceId);
         Editor editor = sharedPrefs.edit();
         editor.putString(Constants.DEVICE_ID, deviceId);
-        editor.commit();
+        editor.apply();
 
         // If running on an emulator
         if (deviceId == null || deviceId.trim().length() == 0
@@ -107,7 +107,7 @@ public class NotificationService extends Service {
                         (new Random(System.currentTimeMillis())).nextLong())
                         .toString();
                 editor.putString(Constants.EMULATOR_DEVICE_ID, deviceId);
-                editor.commit();
+                editor.apply();
             }
         }
         Log.d(LOGTAG, "deviceId=" + deviceId);
